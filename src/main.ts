5318c42e837013dff88fb8f715ae28869ed230f9
Minato1123 <slamdunk0522@gmail.com>
2023-06-15 14:16:10 +0800

    chore: add floating-vue

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import FloatingVue from 'floating-vue'
 
 import App from './App.vue'
 import router from './router'
 
 import 'normalize.css'
+import 'floating-vue/dist/style.css'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(FloatingVue)
 
 app.mount('#app')
