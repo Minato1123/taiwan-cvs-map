dc7129e2d23d89866fb6ad52d90b2d0380394619
Minato1123 <slamdunk0522@gmail.com>
2023-06-16 15:32:44 +0800

    chore: add auto-animate

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import FloatingVue from 'floating-vue'
+import { autoAnimatePlugin } from '@formkit/auto-animate/vue'
 
 import App from './App.vue'
 import router from './router'
@@ -13,5 +14,6 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.use(FloatingVue)
+app.use(autoAnimatePlugin)
 
 app.mount('#app')
