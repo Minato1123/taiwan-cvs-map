4a1824c542665f6b5612b17ebfd8d3c99b6c3fcf
Minato1123 <slamdunk0522@gmail.com>
2023-06-17 13:59:53 +0800

    chore: add deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env sh
+
+rm -rf dist
+
+# abort on errors
+set -e
+
+# build
+pnpm run build
+
+# navigate into the build output directory
+cd dist
+
+cp index.html 404.html
+
+# place .nojekyll to bypass Jekyll processing
+echo > .nojekyll
+
+# if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init
+git checkout -B master
+git add -A
+git commit -m 'deploy'
+
+# if you are deploying to https://<USERNAME>.github.io
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+# if you are deploying to https://<USERNAME>.github.io/<REPO>
+git push -f https://github.com/Minato1123/taiwan-familymart-map.git master:gh-pages
+
+cd -
--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/taiwan-familymart-map/',
   plugins: [
     vue(),
     AutoImport({
